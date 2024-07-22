f1b83848468b195118219165b6960b1ab4378b98
sleekmind <vinayak.vmcs@gmail.com>
2024-07-22 18:55:17 +0530

    yooo

--- week-2/02-nodejs/todoServer.js
+++ week-2/02-nodejs/todoServer.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const cors = require("cors");
+
 const app = express();
 const dataFile = 'todos.json';
-
+app.use(cors());
 app.use(bodyParser.json());
 
 let todos = [];
